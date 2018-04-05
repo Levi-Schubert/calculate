bd08d54c54884aabb93a7aaddf57f62433a63c72
Levi Schubert <me@levischubert.com>
2018-04-05 09:41:04 -0500

    "initial commit"

---
+++ main.js
@@ -0,0 +1,21 @@
+
+function add(int1, int2){
+    let result = int1 + int2
+    console.log(result)
+}
+function subtract(int1, int2){
+    let result = int1 - int2
+    console.log(result)
+}
+
+const calculate = (int1, int2, operation) => {
+    operation(int1,int2);
+}
+
+add(3,5)
+add(9,14)
+subtract(14,6)
+subtract(5,1)
+
+calculate(100, 99, add)
+calculate(100,99,subtract)
